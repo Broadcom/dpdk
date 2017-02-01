51d22b79139a310cd7f3ba69f79aa781c542040c
Ajit Khaparde <ajit.khaparde@broadcom.com>
2017-01-31 18:40:50 -0600

    bnxt: Update tx offload capabilities
    Update the tx offload capabilities of the PMD.
    Signed-off-by: Ajit Khaparde <ajit.khaparde@broadcom.com>

--- drivers/net/bnxt/bnxt_ethdev.c
+++ drivers/net/bnxt/bnxt_ethdev.c
@@ -353,7 +353,12 @@ static void bnxt_dev_info_get_op(struct rte_eth_dev *eth_dev,
 	dev_info->tx_offload_capa = DEV_TX_OFFLOAD_IPV4_CKSUM |
 					DEV_TX_OFFLOAD_TCP_CKSUM |
 					DEV_TX_OFFLOAD_UDP_CKSUM |
-					DEV_TX_OFFLOAD_TCP_TSO;
+					DEV_TX_OFFLOAD_TCP_TSO |
+					DEV_TX_OFFLOAD_OUTER_IPV4_CKSUM |
+					DEV_TX_OFFLOAD_VXLAN_TNL_TSO |
+					DEV_TX_OFFLOAD_GRE_TNL_TSO |
+					DEV_TX_OFFLOAD_IPIP_TNL_TSO |
+					DEV_TX_OFFLOAD_GENEVE_TNL_TSO;
 
 	/* *INDENT-OFF* */
 	dev_info->default_rxconf = (struct rte_eth_rxconf) {
