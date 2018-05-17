1af25575f4cb6c6b725f071ca30ed7cbe7d64c25
Ajit Khaparde <ajit.khaparde@broadcom.com>
2018-05-17 22:26:52 +0530

    net/bnxt: fix Rx checksum flags
    For frames where the hardware is not able to calculate checksum
    we are indicating such frames to be bad. And that is incorrect.
    Indicate PKT_RX_IP_CKSUM_UNKNOWN for such frames.
    
    Signed-off-by: Ajit Khaparde <ajit.khaparde@broadcom.com>

--- drivers/net/bnxt/bnxt_rxr.h
+++ drivers/net/bnxt/bnxt_rxr.h
@@ -43,6 +43,9 @@
 #define RX_CMP_L4_CS_ERR_BITS	rte_cpu_to_le_32 \
 	(RX_PKT_CMPL_ERRORS_L4_CS_ERROR | RX_PKT_CMPL_ERRORS_T_L4_CS_ERROR)
 
+#define RX_CMP_L4_CS_UNKNOWN(rxcmp1)					\
+	    !((rxcmp1)->flags2 & RX_CMP_L4_CS_BITS)
+
 #define RX_CMP_L4_CS_OK(rxcmp1)						\
 	    (((rxcmp1)->flags2 & RX_CMP_L4_CS_BITS) &&		\
 	     !((rxcmp1)->errors_v2 & RX_CMP_L4_CS_ERR_BITS))
@@ -52,6 +55,9 @@
 
 #define RX_CMP_IP_CS_BITS	rte_cpu_to_le_32 \
 	(RX_PKT_CMPL_FLAGS2_IP_CS_CALC | RX_PKT_CMPL_FLAGS2_T_IP_CS_CALC)
+
+#define RX_CMP_IP_CS_UNKNOWN(rxcmp1)					\
+        !((rxcmp1)->flags2 & RX_CMP_IP_CS_BITS)
 
 #define RX_CMP_IP_CS_OK(rxcmp1)						\
 		(((rxcmp1)->flags2 & RX_CMP_IP_CS_BITS) &&	\
